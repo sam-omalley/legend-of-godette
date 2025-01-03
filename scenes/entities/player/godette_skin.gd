d00518e43a1cfd4575544e49a28304074ab25b7c
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-01-03 11:11:08 +1030

    Add blend node to animation tree

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
-@onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var move_state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/Movement/playback")
+
 
 func set_move_state(state_name: String) -> void:
 	move_state_machine.travel(state_name)
+
+func set_move_speed(speed: float, max_speed: float) -> void:
+	animation_tree.set("parameters/Movement/IdleWalkRun/blend_position", remap(speed, 0, max_speed, 0.0, 1.0))
