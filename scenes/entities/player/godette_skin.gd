15e2c2a38aaf3e8dd3002147d414a58dafe0575f
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-01-03 11:38:09 +1030

    Add attack animation

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -9,3 +9,6 @@ func set_move_state(state_name: String) -> void:
 
 func set_move_speed(speed: float, max_speed: float) -> void:
 	animation_tree.set("parameters/Movement/IdleWalkRun/blend_position", remap(speed, 0, max_speed, 0.0, 1.0))
+
+func attack():
+	animation_tree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
