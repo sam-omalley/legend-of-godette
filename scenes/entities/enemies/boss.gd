5a627e39a8dfc3d844a4527e24ac7b875dce5905
Samuel O'Malley <git@sam.omalley.cc>
2025-01-07 20:52:33 +1030

    Add boss attack animations

--- scenes/entities/enemies/boss.gd
+++ scenes/entities/enemies/boss.gd
@@ -1,40 +1,68 @@
 extends Enemy
 
 @export var attack_distance: float = 6.0
+@export var spin_speed_multiplier: float = 3.0
+
+@onready var attack_timer: Timer = %AttackTimer
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var spinning: bool = false
 
 const simple_attacks: Dictionary = {
-    'slice': '2H_Melee_Attack_Slice',
-    'spin': '2H_Melee_Attack_Spin',
-    'range': '1H_Melee_Attack_Stab'
+	'slice': '2H_Melee_Attack_Slice',
+	'spin': '2H_Melee_Attack_Spin',
+	'range': '1H_Melee_Attack_Stab'
 }
 
 func _physics_process(delta: float) -> void:
-    move_to_player(delta)
+	move_to_player(delta)
 
 
 func _on_attack_timer_timeout() -> void:
-    if position.distance_to(player.position) < attack_distance:
-        melee_attack_animation()
-    else:
-        range_attack_animation()
-	# 4 animations
-
-    # 2 melee attacks
-
-    # 2 range attacks
+	attack_timer.wait_time = rng.randf_range(4.0, 5.5)
+	if position.distance_to(player.position) < attack_distance:
+		melee_attack_animation()
+	else:
+		if rng.randi() % 2 == 0:
+			range_attack_animation()
+		else:
+			spin_attack_animation()
 
 func melee_attack_animation() -> void:
-    set_attack_speed(0.5)
-    attack_animation.animation = simple_attacks['slice' if rng.randi() % 2 == 0 else 'spin'] 
-    animation_tree.set('parameters/AttackOneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	set_attack_speed(0.5)
+	attack_animation.animation = simple_attacks['slice' if rng.randi() % 2 == 0 else 'spin']
+	animation_tree.set('parameters/AttackOneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 func range_attack_animation() -> void:
-    stop_movement(0.3, 1.5)
-    set_attack_speed(1.0)
-    attack_animation.animation = simple_attacks['range']
-    animation_tree.set('parameters/AttackOneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	stop_movement(0.3, 1.5)
+	set_attack_speed(1.0)
+	attack_animation.animation = simple_attacks['range']
+	animation_tree.set('parameters/AttackOneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func spin_attack_animation() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "speed_modifier", spin_speed_multiplier, 0.5)
+	tween.tween_method(_spin_transition, 0.0, 1.0, 0.3)
+	attack_timer.stop()
+	spinning = true
+
+	get_tree().create_timer(rng.randf_range(5.0, 10.0)).timeout.connect(func() -> void: _on_area_3d_body_entered(player))
+
+func _spin_transition(value: float) -> void:
+	animation_tree.set('parameters/SpinBlend/blend_amount', value)
 
 func set_attack_speed(speed: float) -> void:
-    animation_tree.set('parameters/TimeScale/scale', speed)
+	animation_tree.set('parameters/TimeScale/scale', speed)
+
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+	if spinning:
+		await get_tree().create_timer(rng.randf_range(1.0, 2.0)).timeout
+		var tween = create_tween()
+		tween.tween_property(self, "speed_modifier", 1.0, 0.5)
+		tween.tween_method(_spin_transition, 1.0, 0.0, 0.3)
+		attack_timer.start()
+		spinning = false
+
+func _on_axe_hitbox_body_entered(body: Node3D) -> void:
+	if is_attacking() or spinning:
+		body.skin.hit()
--- scenes/entities/player/camera_controller.gd
+++ scenes/entities/player/camera_controller.gd
@@ -1,14 +1,14 @@
 extends Node3D
 
-@export var horizontal_acceleration: float = 2.0
-@export var vertical_acceleration: float = 1.0
+@export var horizontal_acceleration: float = 3.0
+@export var vertical_acceleration: float = 1.5
 @export var mouse_acceleration: float = 0.005
 
 @export var limit_x: Vector2 = Vector2(-0.8, -0.2)
 
-#func _process(delta: float) -> void:
-	# var joy_dir = Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
-	# rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
+func _process(delta: float) -> void:
+	var joy_dir = Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
+	rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
