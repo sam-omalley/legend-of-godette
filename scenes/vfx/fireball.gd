58f6723bd295b8c8f2eef7120978f74333ea50fb
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-01-12 21:19:23 +1030

    Set size of fireballs

--- scenes/vfx/fireball.gd
+++ scenes/vfx/fireball.gd
@@ -8,6 +8,7 @@ var max_distance: float = 40
 
 func _ready() -> void:
 	decay_timer.start(max_distance / speed)
+	scale = Vector3.ONE * 0.1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,3 +23,7 @@ func _on_body_entered(body:Node3D) -> void:
 
 func _on_decay_timer_timeout() -> void:
 	queue_free()
+
+func setup(size: float) -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector3.ONE * size, 0.1)
