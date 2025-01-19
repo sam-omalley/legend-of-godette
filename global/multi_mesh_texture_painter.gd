ca654306a06e0f9cc3462572d21b0ac4f8f709f5
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-01-19 13:22:05 +1030

    Implemented CDF-based triangle selection and random face sampling
    - Built a CDF (Cumulative Distribution Function) based on triangle
      areas.
    - Integrated random face selection using the CDF, ensuring correct triangle is sampled based on the weighted areas.
    - Added functionality to support dynamic texture masking with color information, enabling random point sampling in specific regions of the mesh.

--- global/multi_mesh_texture_painter.gd
+++ global/multi_mesh_texture_painter.gd
@@ -1,39 +1,14 @@
 @tool
 extends MultiMeshInstance3D
 
-@export var mask: Texture2D:
-	set(value):
-		mask = value
-		refresh()
-@export var surface: MeshInstance3D:
-	set(value):
-		surface = value
-		refresh()
-@export var mesh_count: int = 100:
-	set(value):
-		mesh_count = value
-		refresh()
-
-@export var colour_mask: Color:
-	set(value):
-		colour_mask = value
-		refresh()
-@export var min_scale: float = 0.2:
-	set(value):
-		min_scale = value
-		refresh()
-@export var max_scale: float = 3.0:
-	set(value):
-		max_scale = value
-		refresh()
-@export var scale_randomisation: float = 0.0:
-	set(value):
-		scale_randomisation = value
-		refresh()
-@export var mask_threshold: float = 0.0:
-	set(value):
-		mask_threshold = value
-		refresh()
+@export var mask: Texture2D
+@export var surface: MeshInstance3D
+@export var mesh_count: int = 100
+@export var colour_mask: Color
+@export var min_scale: float = 0.2
+@export var max_scale: float = 3.0
+@export_range(0.0, 1.0, 0.1) var scale_randomisation: float = 0.0
+@export_range(0.0, 1.0, 0.1) var mask_threshold: float = 0.0
 
 # Hack to get a "reload" button. Never set value of checkbox to true.
 @export var reload: bool = false:
@@ -109,33 +84,9 @@ func point_on_triangle(bary: Vector3, a: Vector3, b: Vector3, c: Vector3) -> Vec
 func interpolate_colour(bary: Vector3, a: Color, b: Color, c: Color) -> Color:
 	return a * bary.x + b * bary.y + c * bary.z
 
-func setup() -> void:
-	if not surface or not surface.is_inside_tree():
-		return
-	# Set this to zero initially. Some parameters (like transform type) can't be
-	# changed if this is > 0.
-	multimesh.instance_count = 0
-	multimesh.transform_format = MultiMesh.TRANSFORM_3D
-
-	var mdt := MeshDataTool.new()
-	mdt.create_from_surface(surface.mesh, 0)
-
-	var image := mask.get_image()
-	if image.is_compressed():
-		image.decompress()
-	
-	var positions: Array = []
-	var scales: Array = []
-
-	#for vtx in range(mdt.get_vertex_count()):
-		#var col = image.get_pixelv(mdt.get_vertex_uv2(vtx) * Vector2(image.get_size()))
-		#col *= colour_mask
-		#var shade: float = max(col.r, col.g, col.b)
-		#if shade > mask_threshold:
-			#var vert = mdt.get_vertex(vtx)
-			#var pos = surface.global_transform * vert
-			#scales.append(shade)
-			#positions.append(pos)
+func build_cdf_with_mask(mdt: MeshDataTool, image: Image) -> Array[float]:
+	var cdf: Array[float] = []
+	var total_weighted_area: float = 0.0
 
 	for face_idx in range(mdt.get_face_count()):
 		var a_idx: int = mdt.get_face_vertex(face_idx, 0)
@@ -150,7 +101,79 @@ func setup() -> void:
 		var b_col: Color = image.get_pixelv(mdt.get_vertex_uv2(b_idx) * Vector2(image.get_size()))
 		var c_col: Color = image.get_pixelv(mdt.get_vertex_uv2(c_idx) * Vector2(image.get_size()))
 
-		#var bcoord: Vector3 = Geometry3D.get_triangle_barycentric_coords(p, a, b, c)
+		# Get triangle area
+		var area = area_of_triangle(a, b, c)
+
+		# Get mask weight
+		var col: Color = (a_col + b_col + c_col) / 3.0
+		col *= colour_mask
+		var mask_weight: float = max(col.r, col.g, col.b)
+
+		# Weighted area (skip if mask weight is 0)
+		if mask_weight > mask_threshold:
+			# Weight by triangle area and colour
+			#total_weighted_area += area * mask_weight
+			# Weight by triangle area, colour is just used as mask
+			total_weighted_area += area
+			cdf.append(total_weighted_area)
+		else:
+			cdf.append(total_weighted_area)
+
+	# Normalize the CDF
+	for i in range(cdf.size()):
+		cdf[i] /= total_weighted_area
+
+	return cdf
+
+func random_weighted_triangle_idx(cdf: Array[float]) -> int:
+	# Generate a random number
+	var r = randf()
+
+	# Use binary search to find the corresponding triangle
+	var left = 0
+	var right = len(cdf) - 1
+	while left < right:
+		var mid: int = floori((left + right) / 2.0)
+		if cdf[mid] < r:
+			left = mid + 1
+		else:
+			right = mid
+
+	return left
+
+func setup() -> void:
+	if not surface or not surface.is_inside_tree():
+		return
+	# Set this to zero initially. Some parameters (like transform type) can't be
+	# changed if this is > 0.
+	multimesh.instance_count = 0
+	multimesh.transform_format = MultiMesh.TRANSFORM_3D
+
+	var mdt := MeshDataTool.new()
+	mdt.create_from_surface(surface.mesh, 0)
+
+	var image := mask.get_image()
+	if image.is_compressed():
+		image.decompress()
+
+	var cdf: Array[float] = build_cdf_with_mask(mdt, image)
+	
+	# Set this as late as possible to avoid Godot errors.
+	multimesh.instance_count = mesh_count
+	for i in range(multimesh.instance_count):
+		var face_idx = random_weighted_triangle_idx(cdf)
+		var a_idx: int = mdt.get_face_vertex(face_idx, 0)
+		var b_idx: int = mdt.get_face_vertex(face_idx, 1)
+		var c_idx: int = mdt.get_face_vertex(face_idx, 2)
+
+		var a: Vector3 = mdt.get_vertex(a_idx)
+		var b: Vector3 = mdt.get_vertex(b_idx)
+		var c: Vector3 = mdt.get_vertex(c_idx)
+
+		var a_col: Color = image.get_pixelv(mdt.get_vertex_uv2(a_idx) * Vector2(image.get_size()))
+		var b_col: Color = image.get_pixelv(mdt.get_vertex_uv2(b_idx) * Vector2(image.get_size()))
+		var c_col: Color = image.get_pixelv(mdt.get_vertex_uv2(c_idx) * Vector2(image.get_size()))
+
 		var bcoord := random_barycentric()
 
 		var pos: Vector3 = point_on_triangle(bcoord, a, b, c)
@@ -158,39 +181,20 @@ func setup() -> void:
 		col *= colour_mask
 		var shade: float = max(col.r, col.g, col.b)
 
-		if shade > mask_threshold:
-			scales.append(shade)
-			positions.append(surface.global_transform * pos)
-
-
-	
-	# Set this as late as possible to avoid Godot errors.
-	multimesh.instance_count = mesh_count
-	for i in range(multimesh.instance_count):
-		#var t: Transform3D = multimesh.get_instance_transform(i)
-		#t.basis = basis
-		#if t.basis.y.angle_to(basis.y) > deg_to_rad(5):
-			#t = t.translated_local(t.basis.y * -100)
-		#var t = Transform3D(basis, Vector3(positions.pick_random()))
-		if len(positions) == 0:
-			print("Error: Not enough positions for instance_count")
-			return
-		var idx = randi_range(0, len(positions) - 1)
-		var t = Transform3D(basis, Vector3(positions[idx]))
+		var t = Transform3D(basis, surface.global_transform * pos)
 		#t = t.scaled_local(Vector3.ONE * randf_range(0.5, 2) * (1.0 - scales[i]))
 
-		var new_scale: float = max_scale * (scales[idx])
-		new_scale += randf_range(-scale_randomisation, scale_randomisation)
+		var new_scale: float = max_scale * easeOutCubic(shade)
+		new_scale += randf_range(-scale_randomisation, scale_randomisation) * max_scale
 		new_scale = clampf(new_scale, min_scale, max_scale)
 
 		t = t.scaled_local(Vector3.ONE * new_scale)
-		t = t.rotated_local(basis.y.normalized(), randf() * PI)
+		t = t.rotated_local(basis.y.normalized(), randf() * 2.0 * PI)
 		multimesh.set_instance_transform(i, t)
-
-		# Ensure we can't use the same position twice
-		positions.remove_at(idx)
-		scales.remove_at(idx)
 
 func refresh() -> void:
 	if Engine.is_editor_hint():
 		setup()
+
+func easeOutCubic(x: float) -> float:
+	return 1.0 - pow(1.0 - x, 3)
